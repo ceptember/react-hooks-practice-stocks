a3197186caf78ed256c52fc53083fc045eb35922
Christy Perozzi <christy.perozzi@gmail.com>
2022-01-10 13:58:53 -0500

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -12,3 +12,24 @@ function App() {
 }
 
 export default App;
+
+/*
+
+App
+  |---Header
+  |---MainContainer *** State: stocks, bought stocks, sorted/filtered stocks  
+    |---SearchBar ***sort/filter behavior, pass state up to MainContainer 
+    |---StockContainer 
+        |---Stock ***pass fetched stocks down as prop | ***event buy a stock and pass up to MainContainer
+    |---PortfolioContainer ***pass bought stocks down from MainContainer
+        |---Stock *** if it's bought, toggle buy behavior
+
+
+x Render all the stocks onto the page. The styling of how a Stock should look like is already in the Stock component.
+Allow a user to buy a stock by clicking on it and when it is bought, it should be added to MyPortfolio.
+Allow a user to sell a stock in their Portfolio by clicking on the stock and it should be removed from their Portfolio.
+Allow a user to sort the list of stocks alphabetically by the ticker name as well as by ascending price.
+Allow a user to filter stocks based on the type of the stock.
+
+
+*/
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({owned, onSell}) {
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {owned.map( (stock) => <Stock key={stock.id} stock={stock} buySell={onSell}/> )}
     </div>
   );
 }
